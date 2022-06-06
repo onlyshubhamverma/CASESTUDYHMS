020f222a3ffe572d7e7c3ddc02e467023e38bc37
onlyshubhamverma <102594568+onlyshubhamverma@users.noreply.github.com>
2022-06-06 22:05:01 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,106 @@
+import { useState, useEffect} from "react";
+import "./App.css";
+import {variables} from './variables.js';
+
+
+
+
+function App() {
+  const initialValues = { name: "", email: "", password: "" , confirmpassword:""};
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Name is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    return errors;
+  };
+  
+
+  return (
+    <div className="App container">
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="ui message success">Signed in successfully</div>) : (
+        <p></p>
+        )}
+     
+      <h3 className="m-3 d-flex justify-content-center"> Group 4 Residency</h3>
+      <form onSubmit={handleSubmit}>
+        <h1>Registration Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Name</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Name"
+              value={formValues.username}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.email}</p>
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.password}</p>
+        
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ userinfo.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+export class userinfo extends Component{
+    render(){
+        return(
+            <div> </div>
+        )
+    }
+
+}
